dcc9e3cd07a603fe1fc97af14d055ce2a81ce717
vagrant <tpirozzini@gmail.com>
2022-02-22 01:26:33 +0000

    add urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -30,6 +30,10 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
